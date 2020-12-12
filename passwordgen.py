95b11f39b6542234fd5dee3361d7080a3fb7a89a
Ore <oreawokoya@cmail.carleton.ca>
2020-12-12 02:31:42 -0500

    Update passwordgen.py
    Added comments to explain functions

--- passwordgen.py
+++ passwordgen.py
@@ -1,6 +1,7 @@
 import string
 import random
 
+#function to ensure there is at least one digit present in the password
 def checkNum (s):
     return any(i.isdigit() for i in s)
 
@@ -8,8 +9,11 @@ def passwordgenerator (length) :
     ##empty string for password 
     password = ''
     l = int(length)
+
+    #list of charcters to choose from
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     
+
     for x in range(l):
         password = password + (random.choice(chars))
     
@@ -26,14 +30,13 @@ def passwordgenerator (length) :
 
 
     
-           
-
             
 def main ():
     print('\n\t\t Welcome to the password generator system')
     print('\t\t Password length can vary from length 8 to 15')
     print('\t\t The minimum recommended length is a length of 10')
     
+    #ensuring the input is a number and is between 8 and 15
     while True :
         length = input("\n Type in the desired length of password in digits: ")
 
@@ -44,6 +47,5 @@ def main ():
         else :
             print('please enter in a value between 8 and 15')
             
-
 
 main ()
